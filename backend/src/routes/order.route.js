f965365634bc0a8aa2d1811c7ec00845f9f4e02f
David Ericson <davidestudofcg@gmail.com>
2026-02-27 13:04:31 -0300

    feat: add order routes

---
+++ backend/src/routes/order.route.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { 
+    addOrderItemController,
+    createOrderController,
+    getOrderController,
+    getOrderItemsController,
+    updateOrderController
+} from "../controllers/order.controller";
+
+const router = Router();
+
+router.post("/", createOrderController);
+router.post("/:orderId/items", addOrderItemController);
+
+router.get("/:id", getOrderController);
+router.get("/:orderId/items", getOrderItemsController)
+
+router.patch("/:id", updateOrderController);
+
+export default router;
